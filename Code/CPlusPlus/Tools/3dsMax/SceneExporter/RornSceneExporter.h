f97ba7cb59ba7d73611256444195aae981b48eb1
ca6jri <ca6jri@gmail.com>
2010-12-07 21:17:45 +0000

    Materials are now owned by the nodes.

--- Code/CPlusPlus/Tools/3dsMax/SceneExporter/RornSceneExporter.h
+++ Code/CPlusPlus/Tools/3dsMax/SceneExporter/RornSceneExporter.h
@@ -25,12 +25,12 @@ public:
 	int DoExport(const TCHAR* name, ExpInterface* ei, Interface* i, BOOL suppressPrompts, DWORD options);
 	BOOL SupportsOptions(int ext, DWORD options);
 private:
-	static void ExportNodeRecursive(INode* parentNode, XMLHierarchyElement& parentElement, std::set<Mtl*>& materialsToExport);
-	static void ExportNode(INode* node, XMLHierarchyElement& nodeElement, std::set<Mtl*>& materialsToExport);
+	static void ExportNodeRecursive(INode* parentNode, XMLHierarchyElement& parentElement);
+	static void ExportNode(INode* node, XMLHierarchyElement& nodeElement);
+	static void ExportMaterial(Mtl* material, XMLHierarchyElement& parentElement);
 	static void ExportMesh(Mesh& mesh, XMLHierarchyElement& meshElement);
 	static void ExportPoint(const char* name, const Point3& point, XMLHierarchyElement& meshElement);
 	static void ExportFace(const char* name, Face& face, XMLHierarchyElement& meshElement);
-	static void ExportMaterials(const std::set<Mtl*>& materialsToExport, XMLHierarchyElement& parentElement);
-	static void ExportMaterial(Mtl* materialToExport, XMLHierarchyElement& materialElement);
+	
 };
 
