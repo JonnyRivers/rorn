8c0fe1206a61d0df72240a565b06ebe11e511bbc
ca6jri <ca6jri@4fafbd65-2c19-2041-7f78-9da6aaec0f04>
2010-12-31 16:44:15 +0000

    Added time manager Refactored model viewer application code Made model rotate about its Y axis.

---
+++ trunk/Code/CPlusPlus/Engine/Subsystems/TimeManager/TimeManager.cpp
@@ -0,0 +1,41 @@
+#include "TimeManager.h"
+
+using namespace Rorn::Engine;
+
+/*static*/ TimeManager& TimeManager::instance_ = TimeManager();// init static instance
+
+/*static*/ TimeManager& TimeManager::GetInstance()
+{
+	return instance_;
+}
+
+TimeManager::TimeManager(void)
+	: firstStep_(true)
+{
+	
+}
+
+void TimeManager::Step(float& timePassed)
+{
+	if(!firstStep_)
+	{
+		LARGE_INTEGER timeAtThisStep;
+		::QueryPerformanceCounter(&timeAtThisStep);
+		timePassed = static_cast<float>(timerFrequency_.QuadPart) / 
+			static_cast<float>(timeAtThisStep.QuadPart - timeAtLastStep_.QuadPart);
+		timeAtLastStep_ = timeAtThisStep;
+
+		// check for debugger induced massive frame times
+		if(timePassed > 0.1f)
+			timePassed = 0.1f;
+	}
+	else
+	{
+		::QueryPerformanceFrequency(&timerFrequency_);
+		::QueryPerformanceCounter(&timeAtStart_);
+		timeAtLastStep_ = timeAtStart_;
+
+		firstStep_ = false;
+		timePassed = 0.0f;
+	}
+}
