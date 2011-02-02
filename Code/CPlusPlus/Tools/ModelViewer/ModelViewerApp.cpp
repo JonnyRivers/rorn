0244cebc32004f9811ed6abb16641b1b353f7e96
ca6jri <ca6jri@4fafbd65-2c19-2041-7f78-9da6aaec0f04>
2011-02-02 00:20:16 +0000

    Made the pool table with balls model. Increased ambient lighting to show off the pool table model.

--- Code/CPlusPlus/Tools/ModelViewer/ModelViewerApp.cpp
+++ Code/CPlusPlus/Tools/ModelViewer/ModelViewerApp.cpp
@@ -73,12 +73,12 @@ BOOL ModelViewerApp::InitInstance(HINSTANCE instanceHandle, const wchar_t* comma
 
 	// This also MUST be done by the client.  So, should we make it part of the Startup()?
 	Vector3 mainLightDirection(0, -sin(Rorn::Maths::PiOver4), sin(Rorn::Maths::PiOver4));
-	Float4 mainLightColor(0.7f, 0.7f, 0.7f, 1.0f);
+	Float4 mainLightColor(0.8f, 0.8f, 0.8f, 1.0f);
 	light_ = RenderManager::GetInstance().CreateLight(mainLightDirection, mainLightColor);
 	RenderManager::GetInstance().SetMainLight(light_);
 
 	// Setup ambient lighting
-	Float4 ambientLightColor(0.1f, 0.1f, 0.1f, 1.0f);
+	Float4 ambientLightColor(0.3f, 0.3f, 0.3f, 1.0f);
 	RenderManager::GetInstance().SetAmbientLightColor(ambientLightColor);	
 
 	return TRUE;
