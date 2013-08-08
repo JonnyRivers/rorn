4d8678694a97ac83e1b17f2f3c8831abd48231a3
ca6jri <ca6jri@4fafbd65-2c19-2041-7f78-9da6aaec0f04>
2013-08-08 13:12:29 +0000

    Added a physics demo "game" to use as a testbed for integrating bullet.

---
+++ trunk/Code/CPlusPlus/Games/PhysicsDemo001/PhysicsDemo001.cpp
@@ -0,0 +1,49 @@
+// PhysicsDemo001.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "PhysicsDemo001.h"
+
+#include "PhysicsDemo001App.h"
+
+int APIENTRY _tWinMain(HINSTANCE instanceHandle,
+                       HINSTANCE previousInstanceHandle,
+                       LPTSTR commandLine,
+                       int cmdShow)
+{
+	UNREFERENCED_PARAMETER(previousInstanceHandle);
+
+	if( wcslen(commandLine) == 0 )
+	{
+		::MessageBox(NULL, L"You must specify a valid model file path on the command line when running the Model Viewer", L"Invalid command line arguments", MB_OK);
+		return FALSE;
+	}
+
+	PhysicsDemo001App application;
+	if (!application.InitInstance(instanceHandle, commandLine, cmdShow))
+	{
+		application.ExitInstance();
+		return FALSE;
+	}
+
+	LoadAccelerators(instanceHandle, MAKEINTRESOURCE(IDC_PHYSICSDEMO001));
+
+	// Main message loop
+    MSG msg = {0};
+    while( WM_QUIT != msg.message )
+    {
+        if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) )
+        {
+            TranslateMessage( &msg );
+            DispatchMessage( &msg );
+        }
+        else
+        {
+            application.Step();
+        }
+    }
+
+	application.ExitInstance();
+
+	return (int) msg.wParam;
+}
